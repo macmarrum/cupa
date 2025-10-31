53119c020e8a24f612f0361f033fa4ec2b77a073
macmarrum <7197131+macmarrum@users.noreply.github.com>
2025-10-31 20:18:17 +0100

    refactor to use generators

--- rsmatcher/ruleset_matcher.py
+++ rsmatcher/ruleset_matcher.py
@@ -15,6 +15,7 @@ class Rule:
     size: str
     key: str
     ALL: ClassVar[str] = 'ALL'
+    COMBO_SEP: ClassVar[str] = '/'
 
 
 @dataclass
@@ -30,30 +31,32 @@ class RuleSetMatcher:
         self._rule_set = rule_set
 
     def matches(self, item: Item):
-        return bool(self._get_count_of_matching_rules(item))
+        for _ in self._gen_rules_matching_combo_color_size_with_highest_prio_and_matching_key(item):
+            return True
+        return False
 
-    def _get_count_of_matching_rules(self, item: Item) -> int:
-        return len(self._get_rules_matching_also_type(item))
+    def _gen_rules_matching_combo_color_size_with_highest_prio_and_matching_key(self, item: Item):
+        for rule in self._gen_rules_matching_combo_color_size_with_highest_prio(item):
+            if rule.key == item.key:
+                yield rule
 
-    def _get_rules_matching_also_type(self, item: Item):
-        return [r for r in self._get_rules_matching_combo_color_size_with_highest_priority(item) if r.key == item.key]
+    def _gen_rules_matching_combo_color_size_with_highest_prio(self, item: Item):
+        matching_rules, max_prio = self._get_rules_matching_combo_color_size__max_prio(item)
+        for rule in matching_rules:
+            if rule.prio == max_prio:
+                yield rule
 
-    def _get_rules_matching_combo_color_size_with_highest_priority(self, item: Item):
-        matching_rules = self._get_rules_matching_combo_color_size(item)
-        if matching_rules:
-            max_priority = max(c.prio for c in matching_rules)
-            return [r for r in matching_rules if r.prio == max_priority]
-        return []
-
-    def _get_rules_matching_combo_color_size(self, item: Item):
+    def _get_rules_matching_combo_color_size__max_prio(self, item: Item):
         matching_rules = []
+        max_prio = None
         for rule in self._rule_set:
             if (item.key in rule.combo
                     and (rule.color == item.color or rule.color == Rule.ALL)
                     and (rule.size == item.size or rule.size == Rule.ALL)
             ):
                 matching_rules.append(rule)
-        return matching_rules
+                max_prio = max(max_prio, rule.prio) if max_prio else rule.prio
+        return matching_rules, max_prio
 
 
 class RulesParser:
@@ -64,5 +67,5 @@ class RulesParser:
         rules = []
         for row in reader:
             combo, priority, color, size, key = row
-            rules.append(Rule(combo=combo.split('/'), prio=int(priority), color=color, size=size, key=key))
+            rules.append(Rule(combo=combo.split(Rule.COMBO_SEP), prio=int(priority), color=color, size=size, key=key))
         return rules
